2e02d6342db109185795f6881080a8e72ccbed7b
Suhani <suhaninegi124@gmail.com>
2025-07-13 07:26:38 +0530

    useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+
+import { useRef, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//stop watch
+function App(){
+  const [time,setTime]=useState(0);
+  let timerRef=useRef(null);
+
+  function startTimer(){
+    timerRef.current=setInterval(()=>{
+      setTime(time=>time+1);
+    },1000);
+  }
+
+  function stopTimer(){
+    clearInterval(timerRef.current);
+    timerRef.current=null;
+  }
+
+  function resetTimer(){
+    stopTimer();
+    setTime(0);
+  }
+
+  return(
+    <div>
+      <h1>StopWatch : {time} seconds</h1>
+      <button onClick={startTimer}>Start</button>
+      <br/>
+      <br/>
+      <button onClick={stopTimer}>Stop</button>
+      <br/>
+      <br/>
+      <button onClick={resetTimer}>Reset</button>
     </div>
   );
 }
+
+// function App() {
+//   let box=useRef(null);
+//   function handleClick(){
+//     box.current.style.backgroundColor="red";
+//   }
+
+
+//   return(
+//     <div>
+//       <div ref={box}>Hello There</div>
+//       <button onClick={handleClick}>Change Color</button>
+
+//     </div>
+//   )
+  // let value=useRef(0);
+  // //let value=0;
+  // const [count,setCount]=useState(0);
+
+  // function handleClick(){
+  //   setCount(count+1);
+  //   value.current=value.current+1;
+  //   console.log(value.current);
+  //   //value=value+1;
+  //   //console.log(value);
+  // }
+
+  
+  // return (
+  //   <div>
+  //     <button onClick={handleClick}>Increment Count</button>
+  //     <div>Count : {count}</div>
+  //   </div>
+  // );
+// }
 
 export default App;
